db35a99e447588a61ad8d99b84d5357245d743ae
rakzz <raka.pradana1103@outlook.co.id>
2023-12-07 11:17:14 +0700

    Recoding

---
+++ requirement.py
@@ -0,0 +1,8 @@
+
+#REQUIREMENT
+print("pip install matplotlib")
+print("pip install seaborn")
+print("pip install pandas(klo blm)")
+print("pip install numpy(klo blm)")
+print("pip install statsmodels")
+print("pip install sns")
